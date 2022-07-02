0fedfeece216c0e936ae03ac2005931a48d1c0c3
hojeong2747 <hejeong2747@gmail.com>
2022-07-02 16:11:33 +0900

    url

--- js/moa.js
+++ js/moa.js
@@ -1,13 +1,16 @@
 
 const $SurveyList = document.querySelector("#SurveyList");
 
+const $detailTitle = document.querySelector("detailTitle");
+const $mainBottom = document.querySelector("#mainBottom");
+
 const fetchSurvey = () => {
     var requestOptions = {
         method: "GET",
     };
 
     fetch(
-        "url",
+        "umcsom.shop:9000",
         requestOptions
     )
         .then((response) => response.json())
@@ -23,13 +26,23 @@ function SurveyListTemplate (data) {
                                 <a id="title1" href="../html/moa1.html" data-id=${data.result.postId}>  ${data.result.title}  </a>
                             </div>
                             <div class="two-container1">
-                                <span id="count1">  ${data.result.content}  </span>
+                                <span id="count1">11개 항목</span>
+                                <span id="type1">선택형</span>
                             </div>
                             <div class="three-container1">
                                 <span id="point1">  ${data.result.point}  </span>
                             </div>
                         </div>
-`;
+    `;
+
+    const dTitle = `<span>  ${data.result.title}  </span>
+    `;
+    
+    const mBottom = `  ${data.result.content}  
+    `;
 
 $SurveyList.insertAdjacentHTML('beforeend', SurveyItem);
+$detailTitle.insertAdjacentHTML('beforeend', dTitle);
+$mainBottom.insertAdjacentHTML('beforeend', mBottom);
 }
+
