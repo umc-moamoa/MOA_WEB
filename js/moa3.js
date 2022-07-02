4361d20a6552c5e85b31d5e0c4ec19041a929d11
hojeong2747 <hejeong2747@gmail.com>
2022-07-02 14:13:31 +0900

    jsinit

---
+++ js/moa3.js
@@ -0,0 +1,35 @@
+
+const $SurveyList = document.querySelector("#SurveytList");
+
+const fetchSurvey = () => {
+    var requestOptions = {
+        method: "GET",
+    };
+
+    fetch(
+        "url",
+        requestOptions
+    )
+        .then((response) => response.json())
+        .then((result) => result.items.map(item => SurveyListTemplate(item)))
+        .catch((error) => console.log("error", error));
+}
+
+fetchSurvey();
+
+function SurveyListTemplate (data) {
+    const SurveyItem = `<div id="main1">
+                            <div class="one-container1">
+                                <a id="title1" href="../html/moa1.html" data-id=${data.result.postId}>  ${data.result.title}  </a>
+                            </div>
+                            <div class="two-container1">
+                                <span id="count1">  ${data.result.content}  </span>
+                            </div>
+                            <div class="three-container1">
+                                <span id="point1">  ${data.result.point}  </span>
+                            </div>
+                        </div>
+`;
+
+$SurveyList.insertAdjacentHTML('beforeend', SurveyItem);
+}
