1b298b5c0df8d925c6d24e75f39d7bb80b8365b3
Skyminn <rucy0716@naver.com>
2022-07-11 15:40:01 +0900

    api 연동 실험(?)

--- js/moa.js
+++ js/moa.js
@@ -10,39 +10,39 @@ const fetchSurvey = () => {
     };
 
     fetch(
-        "umcsom.shop:9000/posts?categoryId=1",
+        "http://umcsom.shop:9000/posts?categoryId=1",
         requestOptions
     )
         .then((response) => response.json())
-        .then((result) => result.items.map(item => SurveyListTemplate(item)))
-        .catch((error) => console.log("error", error));
+        .then((response) => console.log(JSON.parse(response.result)))
+
 }
+
 
 fetchSurvey();
 
-function SurveyListTemplate (data) {
-    const SurveyItem = `<div id="main1">
-                            <div class="one-container1">
-                                <a id="title1" href="../html/moa1.html" data-id=${data.result.postId}>  ${data.result.title}  </a>
-                            </div>
-                            <div class="two-container1">
-                                <span id="count1">11개 항목</span>
-                                <span id="type1">선택형</span>
-                            </div>
-                            <div class="three-container1">
-                                <span id="point1">  ${data.result.point}  </span>
-                            </div>
-                        </div>
-    `;
+// function SurveyListTemplate (data) {
+//     const SurveyItem = `<div id="main1">
+//                             <div class="one-container1">
+//                                 <a id="title1" href="../html/moa1.html" data-id=${data.result.postId}>  ${data.result.title}  </a>
+//                             </div>
+//                             <div class="two-container1">
+//                                 <span id="count1">11개 항목</span>
+//                                 <span id="type1">선택형</span>
+//                             </div>
+//                             <div class="three-container1">
+//                                 <span id="point1">  ${data.result.point}  </span>
+//                             </div>
+//                         </div>
+//     `;
 
-    const dTitle = `<span>  ${data.result.title}  </span>
-    `;
+//     const dTitle = `<span>  ${data.result.title}  </span>
+//     `;
     
-    const mBottom = `  ${data.result.content}  
-    `;
+//     const mBottom = `  ${data.result.content}  
+//     `;
 
-$SurveyList.insertAdjacentHTML('beforeend', SurveyItem);
-$detailTitle.insertAdjacentHTML('beforeend', dTitle);
-$mainBottom.insertAdjacentHTML('beforeend', mBottom);
-}
-
+// $SurveyList.insertAdjacentHTML('beforeend', SurveyItem);
+// $detailTitle.insertAdjacentHTML('beforeend', dTitle);
+// $mainBottom.insertAdjacentHTML('beforeend', mBottom);
+// }
