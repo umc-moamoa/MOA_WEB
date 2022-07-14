93a27dac0782e0a72941745062ad6a6c0815abc6
ddddabi <ii887742@gmail.com>
2022-07-14 19:05:41 +0900

    moa api 성공!!!!!!

--- js/moa.js
+++ js/moa.js
@@ -13,10 +13,9 @@ const fetchSurvey = () => {
         requestOptions
     )
         .then((response) => response.json())
-        .then((webResult) => console.log(webResult))
+        //.then((webResult) => console.log(webResult.result))
         .then((webResult) => webResult.result.map(item => SurveyListTemplate(item)))
         .catch((error) => console.log("error", error));
-        
 }
 
 fetchSurvey();
@@ -24,14 +23,14 @@ fetchSurvey();
 function SurveyListTemplate (data) {
     const SurveyItem = `<div id="main1">
                             <div class="one-container1">
-                                <a id="title1" href="../html/moa1.html" data-id=${data.result.id}>  ${data.result.title}  </a>
+                                <a id="title1" href="../html/moa1.html" data-id=${data.postId}>  ${data.title}  </a>
                             </div>
                             <div class="two-container1">
                                 <span id="count1">11개 항목</span>
                                 <span id="type1">선택형</span>
                             </div>
                             <div class="three-container1">
-                                <span id="point1">  ${data.result.point}  </span>
+                                <span id="point1">  ${data.point}  </span>
                             </div>
                         </div>
     `;
@@ -46,63 +45,3 @@ $SurveyList.insertAdjacentHTML('beforeend', SurveyItem);
 // $detailTitle.insertAdjacentHTML('beforeend', dTitle);
 // $mainBottom.insertAdjacentHTML('beforeend', mBottom);
 }
-
-
-
-
-
-
-
-
-
-
-/****************************************************** */
-// const $SurveyList = document.querySelector("#SurveyList");
-
-// const $detailTitle = document.querySelector("#detailTitle");
-// const $mainBottom = document.querySelector("#mainBottom");
-
-// const fetchSurvey = () => {
-//     var requestOptions = {
-//         method: "GET",
-//     };
-
-//     fetch(
-//         "http://umcsom.shop:9000/posts?categoryId=1",
-//         requestOptions
-//     )
-//         .then((response) => response.json())
-//         .then((result) => result.items.map(item => SurveyListTemplate(item)))
-//         //.then((response) => console.log((JSON.parse(response)).result))
-//         .catch((error) => console.log("error", error));
-
-// }
-
-
-// fetchSurvey();
-
-// function SurveyListTemplate (data) {
-//     const SurveyItem = `<div id="main1">
-//                             <div class="one-container1">
-//                                 <a id="title1" href="../html/moa1.html" data-id=${data.result.postId}>  ${data.result.title}  </a>
-//                             </div>
-//                             <div class="two-container1">
-//                                 <span id="count1">11개 항목</span>
-//                                 <span id="type1">선택형</span>
-//                             </div>
-//                             <div class="three-container1">
-//                                 <span id="point1">  ${data.result.point}  </span>
-//                             </div>
-//                         </div>
-//     `;
-
-//     const dTitle = `<span>  ${data.result.title}  </span>
-//     `;
-    
-//     const mBottom = `  ${data.result.content}  
-//     `;
-
-// $SurveyList.insertAdjacentHTML('beforeend', SurveyItem);
-// // $detailTitle.insertAdjacentHTML('beforeend', dTitle);
-// // $mainBottom.insertAdjacentHTML('beforeend', mBottom);
-// }
