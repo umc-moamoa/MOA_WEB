f1c6b8b72e067034efc3769e308435a51972d871
Skyminn <rucy0716@naver.com>
2022-07-15 15:34:47 +0900

    참여한 설문조사 페이지 레이아웃, js 기본 완료

---
+++ js/participatedSurvey.js
@@ -0,0 +1,35 @@
+const $SurveyList = document.querySelector("#SurveyList");
+
+const fetchParticipate = () => {
+    var requestOptions = {
+        method: "Get",
+    };
+
+    fetch(
+        "http://umcsom.shop:9000/users/2/post",
+        requestOptions
+    )
+        .then((response) => response.json())
+        .then((webResult) => webResult.result.map(item => ParticipateListTemplate(item)))
+        .catch((error) => console.log("error", error));
+}
+
+fetchParticipate();
+
+function ParticipateListTemplate(data) {
+    const ParticipatedItem = `<div id="main1">
+    <div class="one-container1">
+        <a id="title1" href="../html/moa1.html">  ${data.postTitle}  </a>
+    </div>
+    <div class="two-container1">
+        <span id="count1">${data.qcount}개 항목</span>
+        <span id="type1">선택형</span>
+    </div>
+    <div class="three-container1">
+        <span id="point1">  ${data.point}  </span>
+    </div>
+</div>
+`;
+
+$SurveyList.insertAdjacentHTML('beforeend', ParticipatedItem);
+}
