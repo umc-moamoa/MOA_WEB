7934975fbb887344ab1fc7f6c84c9e58d8bba4fa
hojeong2747 <hejeong2747@gmail.com>
2022-07-17 16:39:06 +0900

    버튼 독립적 실행 성공

--- js/makeForm.js
+++ js/makeForm.js
@@ -1,5 +1,30 @@
-var optionNum = 0;
+//var optionNum;
 function addQuestion() {
+
+    function selectedValue() {
+
+        var select = type;
+        var value = select.options[type.selectedIndex].value; 
+        //this, 뭐 등등 필요없이 해당 함수 실행되는 블록에서의 div1, select 등 찾을 수 있음!
+
+        return value;
+    }
+
+    function changeQtype() {
+
+        var thisQs = div1.children;
+        for (var i=1; i<thisQs.length-1; i++) {
+            var thisQinput2 = thisQs[i].firstChild;
+            var thisQvalue = selectedValue();
+            if (thisQvalue == 1) 
+                thisQinput2.type = 'radio';
+            else if (thisQvalue == 2)
+                thisQinput2.type = 'checkbox'; 
+                //여기에 단답형, 장문형 타입도 정의하면 된다. 이건 또 생각해볼 문제,, 단순 타입 변경이 아니라 전체 div를 바꿔야함.
+                // 그냥 바꾸는 게 문제가 아니라 다시 버튼으로 돌아올 수 있어야 함 -> 원래 꺼 아예 지우면 안 됨 or 다시 돌아올 때 다시 생성
+        }
+
+    }
 
     function addOption() {
         //옵션 추가란 누르자마자 placeholder 글 지워지고, 새 옵션 추가란 생기도록
@@ -9,7 +34,14 @@ function addQuestion() {
         div3.className = 'makeOption';
     
         var input2 = document.createElement("input");
-        input2.type = 'radio';
+        var value2 = selectedValue();
+        if (value2 == 1) {
+            input2.type = 'radio';
+        } else if (value2 ==2 ) { // **** 아직도 1번 문항의 selectedValue에 종속되어 있음 -> selectedValue 고쳐서 해결함!
+            input2.type = 'checkbox';
+        }
+
+        input2.className = 'btns'
         input2.name = 'checkOption' + optionNum;
         // input2.value = '4'; 
     
@@ -78,10 +110,26 @@ function addQuestion() {
 
     var type = document.createElement("select");
     type.className = 'Qtype';
+    type.id = 'Qtype'; //id 추가
+    type.onchange = changeQtype; //이벤트 함수 추가/
+/*     type.onchange = function () { 
+        var thisQs = div1.children;
+        for (var i=1; i<thisQs.length-1; i++) {
+            var thisQinput2 = thisQs[i].firstChild;
+            var thisQvalue = selectedValue();
+            if (thisQvalue == 1) 
+                thisQinput2.type = 'radio';
+            else if (thisQvalue == 2)
+                thisQinput2.type = 'checkbox'; 
+        }
+    } */
+    // 이대로 하니까 1번에 종속되지는 않는데, 1번만 변경됨..  -> 별도 함수 다시 
+    // -> 문제는 이게 아니라 전체에서 찾아서 그런 거였음. 해당 블록 안에서 알아서 찾아줌 !
 
     var typeOption1 = document.createElement("option");
     typeOption1.value = 1;
     var optionText1 = document.createTextNode('객관식 질문');
+    typeOption1.defaultSelected; //디폴트 선택
     var typeOption2 = document.createElement("option");
     typeOption2.value = 2;
     var optionText2 = document.createTextNode('체크박스');
@@ -108,144 +156,153 @@ function addQuestion() {
     type.appendChild(typeOption4);
     div2.appendChild(type);
 
+    // type.onchange = changeQtype; //이벤트 함수 추가
+
+    // var value1 = selectedValue(); //return 값 있는 함수는 이렇게 호출하나봐.
+
+    // if (value1 == 1) { //이거 안 써도 plus로 추가될 때는 radio 버튼으로 생긴다. 아래 코드 때문에 ! -> 결국 다 한 블럭.
     //makeOption
     var div3 = document.createElement("div");
-        div3.className = 'makeOption';
-    
-        var input2 = document.createElement("input");
-        input2.type = 'radio';
-        input2.name = 'checkOption' + optionNum;
-        // input2.value = '1';
-    
-        var input3 = document.createElement("input");
-        input3.type = 'text';
-        input3.className = 'optionInput';
-        input3.setAttribute('maxlength', '40');
-        input3.setAttribute('placeholder', '옵션 1');
-        input3.setAttribute('onfocus', 'this.placeholder=""');
-        input3.setAttribute('onblur', 'this.placeholder="옵션 1"');
-    
-        var xbtn = document.createElement("button");
-        xbtn.id = 'xButton';
-        xbtn.type = 'button';
-        xbtn.onclick = function() {
-            //xbtn 누르면 xbtn의 부모 div remove.
-            var parent = this.parentNode;
-            parent.remove();            
-           
-        };
-        var ximage = document.createElement("img");
-        ximage.setAttribute('src', '../image/Group 16.png');
-        ximage.setAttribute('width', '50%');
-    
-        div3.appendChild(input2);
-        div3.appendChild(input3);
-        div1.appendChild(div3);
-        xbtn.appendChild(ximage);
-        div3.appendChild(xbtn);
+    div3.className = 'makeOption';
 
-        //makeOption
-        var div3 = document.createElement("div");
-        div3.className = 'makeOption';
-    
-        var input2 = document.createElement("input");
-        input2.type = 'radio';
-        input2.name = 'checkOption' + optionNum; //name으로 구별을 한다..
-        // input2.value = '2';
-    
-        var input3 = document.createElement("input");
-        input3.type = 'text';
-        input3.className = 'optionInput';
-        input3.setAttribute('maxlength', '40');
-        input3.setAttribute('placeholder', '옵션 2');
-        input3.setAttribute('onfocus', 'this.placeholder=""');
-        input3.setAttribute('onblur', 'this.placeholder="옵션 2"');
-    
-        var xbtn = document.createElement("button");
-        xbtn.id = 'xButton';
-        xbtn.type = 'button';
-        xbtn.onclick = function() {
-            //xbtn 누르면 xbtn의 부모 div remove.
-            var parent = this.parentNode;
-            parent.remove();            
-           
-        };
-        var ximage = document.createElement("img");
-        ximage.setAttribute('src', '../image/Group 16.png');
-        ximage.setAttribute('width', '50%');
-    
-        div3.appendChild(input2);
-        div3.appendChild(input3);
-        div1.appendChild(div3);
-        xbtn.appendChild(ximage);
-        div3.appendChild(xbtn);
+    var input2 = document.createElement("input");
+    input2.type = 'radio'; //새로 추가되는 질문은 다 radio 버튼으로 생긴다. 
+    input2.className = 'btns'
+    input2.name = 'checkOption' + optionNum;
+    // input2.value = '1';
+
+    var input3 = document.createElement("input");
+    input3.type = 'text';
+    input3.className = 'optionInput';
+    input3.setAttribute('maxlength', '40');
+    input3.setAttribute('placeholder', '옵션 1');
+    input3.setAttribute('onfocus', 'this.placeholder=""');
+    input3.setAttribute('onblur', 'this.placeholder="옵션 1"');
+
+    var xbtn = document.createElement("button");
+    xbtn.id = 'xButton';
+    xbtn.type = 'button';
+    xbtn.onclick = function() {
+        //xbtn 누르면 xbtn의 부모 div remove.
+        var parent = this.parentNode;
+        parent.remove();            
+        
+    };
+    var ximage = document.createElement("img");
+    ximage.setAttribute('src', '../image/Group 16.png');
+    ximage.setAttribute('width', '50%');
+
+    div3.appendChild(input2);
+    div3.appendChild(input3);
+    div1.appendChild(div3);
+    xbtn.appendChild(ximage);
+    div3.appendChild(xbtn);
+
+    //makeOption
+    var div3 = document.createElement("div");
+    div3.className = 'makeOption';
+
+    var input2 = document.createElement("input");
+    input2.type = 'radio';
+    input2.className = 'btns'
+    input2.name = 'checkOption' + optionNum; //name으로 구별을 한다..
+    // input2.value = '2';
+
+    var input3 = document.createElement("input");
+    input3.type = 'text';
+    input3.className = 'optionInput';
+    input3.setAttribute('maxlength', '40');
+    input3.setAttribute('placeholder', '옵션 2');
+    input3.setAttribute('onfocus', 'this.placeholder=""');
+    input3.setAttribute('onblur', 'this.placeholder="옵션 2"');
+
+    var xbtn = document.createElement("button");
+    xbtn.id = 'xButton';
+    xbtn.type = 'button';
+    xbtn.onclick = function() {
+        //xbtn 누르면 xbtn의 부모 div remove.
+        var parent = this.parentNode;
+        parent.remove();            
+        
+    };
+    var ximage = document.createElement("img");
+    ximage.setAttribute('src', '../image/Group 16.png');
+    ximage.setAttribute('width', '50%');
+
+    div3.appendChild(input2);
+    div3.appendChild(input3);
+    div1.appendChild(div3);
+    xbtn.appendChild(ximage);
+    div3.appendChild(xbtn);
 
 
-        //addOption
-        var div3 = document.createElement("div");
-        div3.className = 'makeOption';
+    //addOption
+    var div3 = document.createElement("div");
+    div3.className = 'makeOption';
+
+    var input2 = document.createElement("input");
+    input2.type = 'radio';
+    input2.className = 'btns'
+    input2.name = 'checkOption' + optionNum;
+    // input2.value = '3'; 
+
+    var input3 = document.createElement("input");
+    input3.type = 'text';
+    input3.className = 'optionInput';
+    input3.setAttribute('maxlength', '40');
+    input3.setAttribute('placeholder', "옵션 추가 또는 '기타' 추가");
+    // input3.setAttribute('onfocus', 'this.placeholder=""');
+
+    input3.onfocus = addOption;
+    input3.setAttribute('onblur', 'this.placeholder="옵션 추가 또는 \'기타\' 추가"');
+
+    var xbtn = document.createElement("button");
+    xbtn.id = 'xButton';
+    xbtn.type = 'button';
+    xbtn.onclick = function() {
+        //xbtn 누르면 xbtn의 부모 div remove.
+        var parent = this.parentNode;
+        parent.remove();            
+        
+    };
+    var ximage = document.createElement("img");
+    ximage.setAttribute('src', '../image/Group 16.png');
+    ximage.setAttribute('width', '50%');
+
+    div3.appendChild(input2);
+    div3.appendChild(input3);
+    div1.appendChild(div3);
+    xbtn.appendChild(ximage);
+    div3.appendChild(xbtn);
+
+    //Qdelete
+    var div4 = document.createElement("div");
+    div4.className = 'Qdelete';
+
+    var tbtn = document.createElement("button");
+    tbtn.id = 'trash';
+    tbtn.type = 'button';
+    tbtn.onclick = function() {
+        div1.remove();
+        
+        var rank = 1;
+        var surveyElements = document.getElementById("surveyElements");
+        for (var i=0; i<surveyElements.children.length; i++) {
+            var surveyElement = surveyElements.children[i];
+            var question = surveyElement.firstChild;
+            var Qnum = question.firstChild;
+            Qnum.innerHTML = rank + ".";
+            rank++;
+        }
+    };
+
+    var timage = document.createElement("img");
+    timage.setAttribute('src', '../image/Trash can.png');
+    timage.setAttribute('width', '50%');
+
+    div1.appendChild(div4);
+    tbtn.appendChild(timage);
+    div4.appendChild(tbtn);
     
-        var input2 = document.createElement("input");
-        input2.type = 'radio';
-        input2.name = 'checkOption' + optionNum;
-        // input2.value = '3'; 
-    
-        var input3 = document.createElement("input");
-        input3.type = 'text';
-        input3.className = 'optionInput';
-        input3.setAttribute('maxlength', '40');
-        input3.setAttribute('placeholder', "옵션 추가 또는 '기타' 추가");
-        // input3.setAttribute('onfocus', 'this.placeholder=""');
-
-        input3.onfocus = addOption;
-        input3.setAttribute('onblur', 'this.placeholder="옵션 추가 또는 \'기타\' 추가"');
-    
-        var xbtn = document.createElement("button");
-        xbtn.id = 'xButton';
-        xbtn.type = 'button';
-        xbtn.onclick = function() {
-            //xbtn 누르면 xbtn의 부모 div remove.
-            var parent = this.parentNode;
-            parent.remove();            
-           
-        };
-        var ximage = document.createElement("img");
-        ximage.setAttribute('src', '../image/Group 16.png');
-        ximage.setAttribute('width', '50%');
-    
-        div3.appendChild(input2);
-        div3.appendChild(input3);
-        div1.appendChild(div3);
-        xbtn.appendChild(ximage);
-        div3.appendChild(xbtn);
-
-        //Qdelete
-        var div4 = document.createElement("div");
-        div4.className = 'Qdelete';
-
-        var tbtn = document.createElement("button");
-        tbtn.id = 'trash';
-        tbtn.type = 'button';
-        tbtn.onclick = function() {
-            div1.remove();
-            
-            var rank = 1;
-            var surveyElements = document.getElementById("surveyElements");
-            for (var i=0; i<surveyElements.children.length; i++) {
-                var surveyElement = surveyElements.children[i];
-                var question = surveyElement.firstChild;
-                var Qnum = question.firstChild;
-                Qnum.innerHTML = rank + ".";
-                rank++;
-            }
-        };
-
-        var timage = document.createElement("img");
-        timage.setAttribute('src', '../image/Trash can.png');
-        timage.setAttribute('width', '50%');
-
-        div1.appendChild(div4);
-        tbtn.appendChild(timage);
-        div4.appendChild(tbtn);
-
 }
+
