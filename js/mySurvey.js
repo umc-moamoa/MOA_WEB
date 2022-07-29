aa3e93f1a55e2862a1b7e7053d84cc2d5b4e59b4
ddddabi <ii887742@gmail.com>
2022-07-29 17:57:04 +0900

    나의 설문조사 수정

--- js/mySurvey.js
+++ js/mySurvey.js
@@ -15,6 +15,7 @@ const fetchSurvey = () => {
         .then((response) => response.json())
         // .then((webResult) => console.log(webResult.result))
         .then((webResult) => webResult.result.map(item => SurveyListTemplate(item)))
+        .then((webResult) => slick())
         .catch((error) => console.log("error", error));
 }
 
@@ -43,4 +44,17 @@ function SurveyListTemplate (data) {
     </div>
     `;
 $SurveyList.insertAdjacentHTML('beforeend', SurveyItem);
+}
+
+function slick(){
+    $(document).ready(function(){
+        $('.SurveyList').slick({
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            rows:3,
+            infinite:false,
+            prevArrow : $('.prev'), 
+            nextArrow : $('.next'), 
+        });
+    });
 }
