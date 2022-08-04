7a4e7e9f48d770eabb284eff6e8cc201263935b4
hojeong2747 <hejeong2747@gmail.com>
2022-08-04 18:03:34 +0900

    설문 폼 작성 객체

--- js/makeForm2.js
+++ js/makeForm2.js
@@ -1,3 +1,43 @@
 function sortQuestion() {
+
+    // title, content
+    var title = document.getElementById("inputTitle");
+    var content = document.getElementById("inputExplain");
     
+    var whole = new Array();
+    whole.push(title);
+    whole.push(content);
+
+    // postDetails
+    var postDetails = new Array();
+    var postFormat = new Array();
+
+    // 일단 큰 틀은? surveryElement 모두 처리 할 때까지!
+    // question div는 전체 배열로 찾고 해도 될 듯. 어차피 elements 인덱스랑 같은 맥락으로 작동함.
+    // Qinput, Qtype 해당.
+    var elements = document.getElementsByClassName("surveyElement");
+    var questions = document.getElementsByClassName("Qinput");
+    var types = document.getElementsByClassName("Qtype");
+    for (i=0; i<elements.length; i++) {
+        // elements[0]이 문항 1번.
+
+        questions[0].value = "hi";
+        postDetails.push(questions[0].value);
+        postDetails.push(types[0].value); // 1,2,3,4
+
+        // makeoption은 elements[i]의 children 중에서 구별해서 써야함.
+        var options = elements[i].children;
+        postFormat.splice(0, postFormat.length); // postFormat 비우고 추가.
+        for (j=1; j<options.length-1; j++) {
+            var option = options[j].children[1];
+            postFormat.push(option);
+        }
+
+        postDetails.push(postFormat);
+
+    }
+
+    whole.push(postDetails);
+
+    console.log(whole);
 }
