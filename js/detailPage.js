80884f9c65bcd63750b246ca79f7c7978c973191
hojeong2747 <hejeong2747@gmail.com>
2022-08-21 17:14:57 +0900

    detailPage

--- js/detailPage.js
+++ js/detailPage.js
@@ -12,7 +12,6 @@ const fetchDetail = () => {
     })
         .then((response) => response.json())
         .then((webResult) => {
-            login_alert();
             SurveyDetailTemplate(webResult.result);
         })
         .catch((error) => console.log("error", error));
